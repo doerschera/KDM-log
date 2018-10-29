27936eec8bc715b31cc7798ff836f10405c21b92
Aly Doerscher <adoerscher@workwithopal.com>
2018-10-28 18:20:27 -0700

    connect server and db

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const typeDefs = `
-  type Query {
-    info: String!
-    survivors: [Survivor!]!
-  }
-
-  type Survivor {
-    id: ID!
-    name: String!
-    gender: String!
-  }
-`;
-
-const survivors = [{
-  id: 1,
-  name: 'Staby',
-  gender: 'm',
-}];
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
     info: () => 'Have a bad time all the time',
-    survivors: () => survivors,
+    survivors: (root, args, context, info ) => context.db.query.survivors({}, info),
+  },
+  Mutation: {
+    survivor: (root, args, context, info) => {
+      return context.db.mutation.createSurvivor({
+        data: {
+          name: args.name || 'Survivor',
+          gender: args.gender,
+        },
+      }, info);
+    },
   },
   Survivor: {
     id: (root) => root.id,
@@ -32,8 +24,17 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/aly-doerscher-4d5fa2/kdm/dev',
+      secret: 'darklantern',
+      ebug: true,
+    })
+  }),
 });
 
 server.start(() => console.log('Server is running on http://localhost:4000'));

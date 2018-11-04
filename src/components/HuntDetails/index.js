1fa1772403294ea076ff9717e1d2e6beb46f87b4
Aly Doerscher <adoerscher@workwithopal.com>
2018-11-03 22:05:58 -0700

    More input styles

--- src/components/HuntDetails/index.js
+++ src/components/HuntDetails/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { string, object } from 'prop-types';
+import Button from '../Button';
 import HitLocationInput from '../HitLocationInput';
 
 import './styles.scss';
@@ -15,6 +16,8 @@ const HuntDetails = ({
         {survivor.hitLocations.map(location => (
           <HitLocationInput hitLocation={location} key={`${id}-${location.type}`} />
         ))}
+        <Button title="Update Armor" />
+        <Button title="Reset Values" />
       </div>
     </div>
   );
